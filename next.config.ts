da441e91577dfec7412f7e15b4d4046cbcfe6a9c
Eric R <earechichi@gmail.com>
2025-10-05 21:01:01 +1100

    Force disable ESLint

--- next.config.ts
+++ next.config.ts
@@ -7,15 +7,30 @@
 // export default nextConfig;
 
 
+// const nextConfig = {
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has TypeScript errors.
+//     ignoreBuildErrors: true,
+//   },
+// }
+
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has TypeScript errors.
     ignoreBuildErrors: true,
   },
+  experimental: {
+    esmExternals: false
+  }
 }
+
+module.exports = nextConfig
+
